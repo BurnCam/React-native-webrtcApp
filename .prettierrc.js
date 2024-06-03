f1ddb7efc15253df1cd8f72ea8766ffa7c7ef27a
Orion <orion.wilmerding@burncammedical.com>
2024-06-03 09:54:38 -0400

    Init daily co example

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
   bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
 };
--- index.js
+++ index.js
@@ -2,10 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Enable debug logs
+/*window.localStorage = window.localStorage || {};
+window.localStorage.debug = '*';
+window.localStorage.getItem = (itemName) => {
+  console.log('Requesting the localStorage item ', itemName);
+  return window.localStorage[itemName];
+};*/
 
-console.disableYellowBox = true;
+import { AppRegistry } from 'react-native';
+import App from './src/components/App/App';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
